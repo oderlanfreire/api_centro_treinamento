fc0f0c842c9ecfffc8659ae95687bd79e56f1569
Oderlan <oderlanfreire@gmail.com>
2024-05-06 16:07:57 -0300

    feature: modificação nas models, inserção do unique

--- api/categorias/models.py
+++ api/categorias/models.py
@@ -10,4 +10,4 @@ class CategoriaModel(BaseModel):
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
     atleta: Mapped['AtletaModel'] = relationship(back_populates="categoria")
-    atleta_id: Mapped[int] = mapped_column(ForeignKey("atleta.pk_id"))    
+       
